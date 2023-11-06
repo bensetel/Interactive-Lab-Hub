098b4ce665dfb5943dc1b06b561be80b0be7a5fa
HaukeCornell <hgs52@cornell.edu>
2023-11-06 11:45:31 -0500

    new stuff

--- Lab 5/theramin.py
+++ Lab 5/theramin.py
@@ -11,6 +11,7 @@ from mingus.midi import fluidsynth
 import mingus.core.notes as notes
 from mingus.containers import NoteContainer
 from mingus.containers import Note
+from mingus.containers import Bar
 
 def play_audio():
     command = ['./loop_audio.sh']
@@ -18,17 +19,28 @@ def play_audio():
     print('Looping audio playback - press q to quit')
     return process  # Return the process object
 
-def play_note(note_num):
-    fluidsynth.play_Note(note_num,0,100)
-    
+# def play_note(note_num):
+#     print(int(note_num))
+#     print('\n')
+#     fluidsynth.play_Note(note_num,0,100)
 
+def display_note(img, note_num, minX, maxX, R, G, B):
+    cv2.rectangle(img, (minX, 150), (maxX, 400), (0, 0, 0), 3)
+    cv2.rectangle(img, (minX, 400 - int((int(note_num)/95 * 250))), (maxX, 400), (R, G, B), cv2.FILLED)
+
+    
 
 
 def main():
     m = alsaaudio.Mixer(control='Speaker', cardindex=3)
     m.setvolume(100) 
-    fluidsynth.init('/usr/share/sounds/sf2/FluidR3_GM.sf2',"alsa")
-    fluidsynth.set_instrument(0, 1)
+    s = fluidsynth.init('/usr/share/sounds/sf2/FluidR3_GM.sf2',"alsa")
+    
+    #fluidsynth.set_instrument(0, 1) #piano on channel 0
+    fluidsynth.set_instrument(1, 50) #strings on channel 1
+    #fluidsynth.set_instrument(1, 61) #brass section on channel 2
+    
+    
 
     ################################
     wCam, hCam = 640, 480
@@ -46,9 +58,14 @@ def main():
     volBar = 400
     volPer = 0
 
-    min_note = 1
-    max_note = 88
-    
+    piano_min = 5
+    piano_max = 107
+    strings_min = 0
+    strings_max = 107
+    brass_min = 0
+    brass_max = 107
+
+    start = time.time()
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
@@ -64,73 +81,61 @@ def main():
 
             cx, cy = (thumbX + pointerX) // 2, (thumbY + pointerY) // 2
 
-            cv2.circle(img, (thumbX, thumbY), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (pointerX, pointerY), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (middleX, middleY), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (ringX, ringY), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (pinkyX, pinkyY), 15, (255, 0, 255), cv2.FILLED)
-            cv2.line(img, (thumbX, thumbY), (pointerX, pointerY), (255, 0, 255), 3)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            #thumb
+            # cv2.circle(img, (thumbX, thumbY), 15, (0, 0, 0), cv2.FILLED)
 
-            len_calc = lambda x1,y1,x2,y2: math.hypot(x2 - x1, y2 - y1)
-            length = len_calc(thumbX,thumbY,pointerX,pointerY)
-            length1 = len_calc(pointerX,pointerY,middleX,middleY)
-            length2 = len_calc(middleX, middleY, ringX, ringY)
-            length3 = len_calc(ringX, ringY, pinkyX, pinkyY)
-            length4 = len_calc(thumbX,thumbY, ringX, ringY)
-            print(length1,length2,length3)
+            #pointer
+            cv2.circle(img, (pointerX, pointerY), 15, (255, 0, 0), cv2.FILLED)
 
-            """
-            condition = length>100 and length1>100 and length2<100 and length3>100 and length4<100
-            if condition:
-                m.setvolume(0)
-                volPer = 0
-                volBar = 400
-                print("CONDITION")
-                cv2.putText(img, 'quiet coyote!', (40, 70), cv2.FONT_HERSHEY_COMPLEX,
-                    1, (255, 255, 255), 3)
+            #middle
+            cv2.circle(img, (middleX, middleY), 15, (0, 0, 255), cv2.FILLED)
+
+            #ring
+            # cv2.circle(img, (ringX, ringY), 15, (0, 0, 0), cv2.FILLED)
+
+            #pinky
+            cv2.circle(img, (pinkyX, pinkyY), 15, (0, 255, 0), cv2.FILLED)
+            
+            
+            
+            note_1 = Note().from_int(int(np.interp(pointerY, [50, 300], [piano_max, piano_min])))
+            note_2 = Note().from_int(int(np.interp(middleY, [50, 300], [piano_max, piano_min])))
+            note_3 = Note().from_int(int(np.interp(pinkyY, [50, 300], [piano_max, piano_min])))
+
+            print('index note: ' + str(int(note_1)) + ' middle note: ' + str(int(note_2)) + ' pinky note: ' + str(int(note_3)))
+            print('\n')
+
+
+            #display pointer
+            display_note(img, note_1, 30, 45, 255, 0, 0)
+
+            #display middle
+            display_note(img, note_2, 45, 60, 0, 0, 255)
+
+            #display pinky
+            display_note(img, note_3, 60, 75, 0, 255, 0)
+
+            now = time.time()
+            if now - start > 5:
+                fluidsynth.control_change(1, 64, 0)
+                start = time.time()
             else:
-                vol = np.interp(length, [50, 300], [minVol, maxVol])
-                volBar = np.interp(length, [50, 300], [400, 150])
-                volPer = np.interp(length, [50, 300], [0, 100])
-                m.setvolume(int(vol))
-
-            print(int(length), vol)
-            """
-            """
-            print('='*100)
-            print('positions:')
-            li = [thumbX, thumbY, middleX, middleY, ringX, ringY, pinkyX, pinkyY]
-            for l in li:
-                print(l)
-                print('--')
+                fluidsynth.control_change(1, 64, 127)
             
-            """
+            c = Bar('C', (4, 4))
+            c.place_notes(note_1, 4)
+            c.place_notes(note_2, 4)
+            c.place_notes(note_3, 4)
+            c.place_rest(4)
+            fluidsynth.play_Bar(c, 1, bpm=80)
             
-            note_1 = Note().from_int(int(np.interp(thumbY, [50, 300], [min_note, max_note])))
-            note_2 = Note().from_int(int(np.interp(middleY, [50, 300], [min_note, max_note])))
-            note_3 = Note().from_int(int(np.interp(pointerY, [50, 300], [min_note, max_note])))
-            n = NoteContainer([note_1, note_2, note_3])
+
             
-            fluidsynth.play_NoteContainer(n, 0, 100)
-            #fluidsynth.play_Note(int(note_1), 0, 100)
-            #fluidsynth.play_Note(int(note_2), 0, 100)
-            #fluidsynth.play_Note(int(note_3), 0, 100)
-            
-            if length < 50:
-                cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-
-        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
-        cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-        cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                    1, (255, 0, 0), 3)
-
-
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-                    1, (255, 0, 0), 3)
+        # cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+        #             1, (255, 0, 0), 3)
 
         cv2.imshow("Img", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
@@ -141,3 +146,5 @@ def main():
     cv2.destroyAllWindows()
 
 
+
+#main()
