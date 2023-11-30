5f7a9bc5485b80fb251dd65e9363d1ecd7bf693e
HaukeCornell <hgs52@cornell.edu>
2023-11-29 22:18:52 -0500

    its alive!

--- final/detect_landmarks.py
+++ final/detect_landmarks.py
@@ -17,8 +17,12 @@ import queue
 
 
 model_path = '/home/ben/Interactive-Lab-Hub/final/pose_landmarker_lite.task'
-global DETECTING
-DETECTING = False
+
+global voter
+voter = 0
+
+global iters
+iters = 0
 
 class HumanPoseDetection:
     def __init__(self):
@@ -43,10 +47,11 @@ class HumanPoseDetection:
             port=8883)
         self.topic = 'IDD/cool_table/robit'
         self.init_positions = []
-        
+        self.threshold = 0.4
     def callback(self, result, output_image, timestamp_ms):
-        global DETECTING
-        DETECTING = True
+        global iters
+        global voter
+
         pl = result.pose_landmarks
         print("-"*100)
         #print("pose landmarks:", pl)
@@ -59,36 +64,63 @@ class HumanPoseDetection:
                 print('initing')
                 self.init_positions = pl[0]
             else:
-                left_change = np.abs(pl[0][11].y - self.init_positions[11].y)
-                right_change = np.abs(pl[0][12].y - self.init_positions[12].y)
-                if left_change > right_change:
+                left_change = np.abs(pl[0][13].y - self.init_positions[13].y)
+                right_change = np.abs(pl[0][14].y - self.init_positions[14].y)
+                print('left_change:', left_change)
+                print('right_change:', right_change)
+
+                """
+                x_left_change = np.abs(pl[0][11].x - self.init_positions[11].x)
+                x_right_change = np.abs(pl[0][12].x - self.init_positions[12].x)
+                print('x_left_change:', x_left_change)
+                print('x_right_change:', x_right_change)
+                """
+                if iters > 10:
+                    voter = 0
+                    iters = 0
+                if (left_change < self.threshold) and (right_change < self.threshold):
+                    print('threshold not met')
+
+                elif left_change > right_change:
                     print('left!')
-                    self.client.publish(self.topic, 'left')
+                    if voter <7:
+                        voter += 1
+                    print('voter is:', voter)
                 else:
                     print('right!')
+                    if voter > -7:
+                        voter -= 1
+                    print('voter is:', voter)
+                if voter >= 5:
+                    self.client.publish(self.topic, 'left')
+                    print('*'*10)
+                    print('sent left')
+                elif voter <= -5:
+                    print('*'*10)
+                    print('sent right')
                     self.client.publish(self.topic, 'right')
-
-        DETECTING = False
+                else:
+                    print('voter is:', voter)
+                    
+                print('iters is:', iters)
+                iters += 1
+                
+                #self.init_positions = pl[0]
+                
         return
 
         
     def detect_pose(self):
-        global DETECTING
         print("detecting pose")
         cap = cv2.VideoCapture('/dev/video0')
         print('camera opened')
         with self.PoseLandmarker.create_from_options(self.options) as landmarker:
             while cap.isOpened():
-                if DETECTING:
-                    print('detecting...')
-                    time.sleep(1)
-                    continue
                 _, image = cap.read()
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
                 frame_timestamp_ms = int(time.time() * 1000)
                 landmarker.detect_async(mp_image, frame_timestamp_ms)
-
-
+                time.sleep(0.05)
 def main():
     HPD_ = HumanPoseDetection()
     HPD_.detect_pose()
